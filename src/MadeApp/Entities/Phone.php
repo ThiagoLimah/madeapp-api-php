02bf8bd049f5e44c210f693593de7979fa809556
Thiago Lima <thiagolimaes@gmail.com>
2016-04-04 14:34:40 -0300

    Implementada entidade Telefone.

---
+++ src/MadeApp/Entities/Phone.php
@@ -0,0 +1,101 @@
+<?php namespace MadeApp\Entities;
+
+/**
+ * Class Phone
+ *
+ * Classe para representar a entidade Telefone.
+ *
+ * @package MadeApp\Entities
+ * @author Thiago A. de Lima <thiagolimaes@gmail.com>
+ * @version 1.0
+ */
+class Phone
+{
+    /**
+     * Código
+     * @var int
+     */
+    private $code;
+
+    /**
+     * Número
+     * @var string
+     */
+    private $number;
+
+    /**
+     * Contato
+     * @var string
+     */
+    private $contact;
+
+    /**
+     * Observações
+     * @var string
+     */
+    private $obs;
+
+    /**
+     * @return int
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param int $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param string $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @param string $contact
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObs()
+    {
+        return $this->obs;
+    }
+
+    /**
+     * @param string $obs
+     */
+    public function setObs($obs)
+    {
+        $this->obs = $obs;
+    }
+}
