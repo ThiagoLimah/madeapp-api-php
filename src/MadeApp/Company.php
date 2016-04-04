67668aa7cf71f66e4992b160d9fba102d435a8ef
Thiago Lima <thiagolimaes@gmail.com>
2016-04-04 14:54:25 -0300

    Implementação da classe Empresa.

--- src/MadeApp/Company.php
+++ src/MadeApp/Company.php
@@ -34,8 +34,20 @@ class Company extends Helper
 
         if (1 === (int) $response->status) {
             $company = new \MadeApp\Entities\Company();
-
-
+            $company->setCode($response->company->codigo);
+            $company->setType($response->company->especie);
+            $company->setName($response->company->razao_social);
+            $company->setSimpleName($response->company->nome_fantasia);
+            $company->setCnpj($response->company->cnpj);
+            $company->setDescription($response->company->descricao);
+            $company->setKeywords($response->company->keywords);
+            $company->setEmail($response->company->email);
+            $company->setWebsite($response->company->website);
+            $company->setLogo($response->company->logo_marca);
+            $company->setVideo($response->company->video);
+            $company->setFbFanPage($response->company->fb_fan_page);
+            $company->setGoogleAnalytics($response->company->google_analytics);
+            $company->setZopimCode($response->company->zopim_code);
 
             return $company;
         }
