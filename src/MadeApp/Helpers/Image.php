4170b13e8e413a7addea622916c78f8277dc0a4a
Thiago Lima <thiagolimaes@gmail.com>
2016-04-05 17:37:10 -0300

    Atualizações.

---
+++ src/MadeApp/Helpers/Image.php
@@ -0,0 +1,55 @@
+<?php namespace MadeApp\Helpers;
+
+/**
+ * Class Image
+ *
+ * Helper para manipulação de arquivos de imagem.
+ *
+ * @package MadeApp\Helpers
+ * @author Thiago A. de Lima <thiagolimaes@gmail.com>
+ * @version 1.0
+ */
+class Image
+{
+    /**
+     * Cria uma miniatura da imagem nas dimensões solicitadas.
+     *
+     * @param string $file
+     * @param int    $width
+     * @param int    $height
+     *
+     * @return string
+     */
+    public static function CreateThumbnail($file, $width = 800, $height = 600)
+    {
+        $mode = \Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND;
+        $size = new \Imagine\Image\Box($width, $height);
+
+        $thumbnail   = \Orchestra\Imagine\Facade::open($file)->thumbnail($size, $mode);
+        $destination = public_path() . "/img/{$width}-{$height}/";
+        $fileName    = self::GetFileName($file);
+
+        if (!file_exists($destination)) {
+            @mkdir($destination, 0777);
+        }
+
+        $thumbnail->save($destination . $fileName);
+
+        return url($destination . $fileName);
+    }
+
+    /**
+     * Retorna o nome da imagem.
+     *
+     * @param string $file
+     *
+     * @return string
+     */
+    public static function GetFileName($file)
+    {
+        $explodedName = (array) explode('/', trim($file));
+        $reversedName = array_reverse($explodedName);
+
+        return $reversedName[0] or '';
+    }
+}
