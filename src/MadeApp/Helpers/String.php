043840d44c15db6920aee87d9d929018ad0b2164
Thiago Lima <thiagolimaes@gmail.com>
2016-04-05 21:03:57 -0300

    Implementação de novas funções.

--- src/MadeApp/Helpers/String.php
+++ src/MadeApp/Helpers/String.php
@@ -48,6 +48,59 @@ class Strings
     }
 
     /**
+     * Limpa a string informada removendo todos os acentos.
+     *
+     * @param string $str
+     * @param string $slug
+     *
+     * @return string
+     */
+    public static function CleanString($str, $slug = false)
+    {
+        $str = self::toLowerCase($str);
+        $str = utf8_decode($str);
+
+        // Código ASCII das vogais
+        $ascii['a'] = range(224, 230);
+        $ascii['e'] = range(232, 235);
+        $ascii['i'] = range(236, 239);
+        $ascii['o'] = array_merge(range(242, 246), array(240, 248));
+        $ascii['u'] = range(249, 252);
+
+        // Código ASCII dos outros caracteres
+        $ascii['b'] = array(223);
+        $ascii['c'] = array(231);
+        $ascii['d'] = array(208);
+        $ascii['n'] = array(241);
+        $ascii['y'] = array(253, 255);
+
+        foreach ($ascii as $key => $item) {
+            $accentuation = "";
+
+            foreach ($item AS $code) {
+                $accentuation .= chr($code);
+            }
+
+            $change[$key] = "/[{$accentuation}]/i";
+        }
+
+        $str = preg_replace(array_values($change), array_keys($change), $str);
+
+        if ($slug) {
+            /* Troca tudo que não for letra ou número por um caractere ($slug) */
+            $str = preg_replace("/[^a-z0-9]/i", $slug, $str);
+
+            /* Tira os caracteres ($slug) repetidos */
+            $str = preg_replace("/{$slug}{2,}/i", $slug, $str);
+
+            /* Remove os caracteres ($slug) do início/fim da string */
+            $str = trim($str, $slug);
+        }
+
+        return $str;
+    }
+
+    /**
      * Converte o encoding de uma string para UTF-8.
      *
      * @param string $str
@@ -56,6 +109,34 @@ class Strings
      */
     public static function toUTF8($str)
     {
-        return mb_convert_encoding((string) $str, 'UTF-8', mb_list_encodings());
+        return mb_convert_encoding((string) $str, "UTF-8", mb_list_encodings());
+    }
+
+    /**
+     * Converte a string informada para caixa baixa.
+     *
+     * @param string $str
+     *
+     * @return string
+     */
+    public static function toLowerCase($str)
+    {
+        $str = self::reSanitize($str);
+
+        return mb_strtolower(self::toUTF8($str), "UTF-8");
+    }
+
+    /**
+     * Converte a string informada para caixa alta.
+     *
+     * @param string $str
+     *
+     * @return string
+     */
+    public static function toUpperCase($str)
+    {
+        $str = self::reSanitize($str);
+
+        return mb_strtoupper(self::toUTF8($str), "UTF-8");
     }
 }
